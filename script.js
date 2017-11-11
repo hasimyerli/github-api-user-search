d2ec700c85f5d529b38cb2007411ff99ae3a6499
hasimyerli <hasimyerli_55@hotmail.com>
2017-11-12 01:52:34 +0300

    loading bar and bug fixed

--- script.js
+++ script.js
@@ -3,15 +3,36 @@ var app = new Vue({
   data: {
     query:"",
     results: false,
-    userNameLength: 9
+    userNameLength: 9,
+    loading: false,
+    listShow:false,
+    messageShow:false,
+    display:"none !important",
+    errorMessage:null
   },
   methods: {
     search: function(){
+      this.loading = true;
+      this.listShow = false;
+      this.messageShow = false;
+      this.display = "block !important";
       fetch(`https://api.github.com/search/users?q=${this.query}`)
       .then(results => results.json())
       .then(results => {
-        this.$set(this, 'results', results)
+        if (!results.errors) {
+          this.loading = false;
+          this.listShow = true;
+          this.$set(this, 'results', results);
+        } else {
+          this.loading = false;
+          this.messageShow = true;
+          this.errorMessage = "Not Found User";
+          return;
+        }
       });
+    },
+    info: function(url){
+      console.log("This section will contain user information.");
     }
   }
 });
